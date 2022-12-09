ebfd41605c9640f8345b4db905970a487559f9c9
saidsoud72 <said.omar@student.moringaschool.com>
2022-12-09 15:18:30 +0300

    add status of created session controllers

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_12_04_145856) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "plpgsql" 
 
   create_table "dogs", force: :cascade do |t|
     t.string "name"
